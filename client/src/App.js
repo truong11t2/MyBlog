32e8781065bc6b867eff67dbb3ec78d2427efd59
truong11t2 <truong11t2@gmail.com>
2023-04-27 19:40:13 +0900

    implement nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-function App() {
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const App = () => {
   return (
-    <div>App</div>
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes></Routes>
+        </main>
+      </Router>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
